139f917afe711231c57f6c408a066c3656b641d2
hyuntae <bht9011@gmail.com>
2022-08-04 08:59:35 +0900

    study before work 20220804

--- src/main/java/hello/servlet/ServletApplication.java
+++ src/main/java/hello/servlet/ServletApplication.java
@@ -3,6 +3,9 @@ package hello.servlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @ServletComponentScan // 서블릿 자동 등록
 @SpringBootApplication
@@ -11,5 +14,15 @@ public class ServletApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ServletApplication.class, args);
 	}
+
+	/**
+	 * spring.mvc.view.prefix=/WEB-INF/views/
+	 * spring.mvc.view.suffix=.jsp
+	 * 를 application.properties에 등롱하면 일어나는 일
+	 */
+//	@Bean
+//	ViewResolver internalResourceViewResolver() {
+//		return new InternalResourceViewResolver("/WEB-INF/views/", ".jsp");
+//	}
 
 }
